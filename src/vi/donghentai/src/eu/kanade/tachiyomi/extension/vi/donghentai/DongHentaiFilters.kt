48ea7fed8231802920440a3db6967f365fb114a9
dowin <thang.do@savvycomsoftware.com>
2026-08-08 14:43:42 +0700

    update dongHentai

--- src/vi/donghentai/src/eu/kanade/tachiyomi/extension/vi/donghentai/DongHentaiFilters.kt
+++ src/vi/donghentai/src/eu/kanade/tachiyomi/extension/vi/donghentai/DongHentaiFilters.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.extension.vi.damconuong
+package eu.kanade.tachiyomi.extension.vi.donghentai
 
 import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
